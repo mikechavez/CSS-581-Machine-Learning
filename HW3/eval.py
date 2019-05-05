23224882f048871a943327275e15e1209271a6c5
Mike <mchavez3@uw.edu>
2019-05-05 14:38:30 -0700

    Resaved

--- HW3/eval.py
+++ HW3/eval.py
@@ -7,9 +7,9 @@ grnd = pd.read_csv('training_groundtruth.csv')
 print(imp.info())
 print(grnd.info())
 
-# not yet working : (
-# for column in imp:
-#     rsme = ((imp[column] - grnd[column]) ** 2).mean() ** .5
-#     print ("RSME for ", column, ": ", rsme)
+# not yet working
+for column in imp:
+    rsme = ((imp[column] - grnd[column]) ** 2).mean() ** .5
+    print ("RSME for ", column, ": ", rsme)
 
 
