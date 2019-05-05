b5722283b0f662f501071e83125b174d6c20b19f
Mike <mchavez3@uw.edu>
2019-05-05 14:37:04 -0700

    Calculate RSME

--- HW3/eval.py
+++ HW3/eval.py
@@ -1,9 +1,15 @@
 import pandas as pd
+from sklearn import metrics
 
 imp = pd.read_csv('training_with_imputed.csv')
 grnd = pd.read_csv('training_groundtruth.csv')
 
+print(imp.info())
+print(grnd.info())
+
 # not yet working : (
-for column in imp:
-    rsme = ((imp[column] - grnd[column]) ** 2).mean() ** .5
-    print ("RSME for ", column, ": ", rsme)
+# for column in imp:
+#     rsme = ((imp[column] - grnd[column]) ** 2).mean() ** .5
+#     print ("RSME for ", column, ": ", rsme)
+
+
