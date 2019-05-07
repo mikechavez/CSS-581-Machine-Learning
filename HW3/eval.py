09c26d1da8545e88ebccc5eeb312786470d67ca0
Mike <mchavez3@uw.edu>
2019-05-06 20:42:34 -0700

    Add sklearn RMSE results

--- HW3/eval.py
+++ HW3/eval.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn import metrics
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 imp = pd.read_csv('training_with_imputed.csv')
 grnd = pd.read_csv('training_groundtruth.csv')
@@ -22,14 +22,39 @@ print(indices_missing)
 imp = imp.drop(indices_missing['missing_rows'])
 grnd = grnd.drop(indices_missing['missing_rows'])
 
+imp.to_csv(r'./imputed_removed_rows.csv', index=None, header=True)
+grnd.to_csv(r'./training_groundtruth_removed_rows.csv', index=None, header=True)
+
+
+
 # Validate Delete was successful
 print(imp.info())
 print(grnd.info())
 
 
-# not yet working
+
+
 for column in imp:
-    rsme = ((imp[column] - grnd[column]) ** 2).mean() ** .5
-    print ("RSME for ", column, ": ", rsme)
+    n = imp.shape[0]
+    max = grnd[column].max()
+    min = grnd[column].min()
+
+    rmse = (((imp[column] - grnd[column]) / (max-min)) ** 2).mean() ** .5
+    print ("RSME for ", column, ": ", rmse)
+
+print("\n\n\n")
+
+for column in imp:
+    max = grnd[column].max()
+    min = grnd[column].min()
+    ground_truth = grnd[column]
+    imputed_data = imp[column]
+    # print("ground truth: ", ground_truth)
+    # print("imputed: ", imputed_data)
+
+    mse = (mean_squared_error(ground_truth, imputed_data))
+    rmse = (mse / (max-min)) ** .5
+
+    print ("RSME for ", column, ": ", rmse)
 
 
