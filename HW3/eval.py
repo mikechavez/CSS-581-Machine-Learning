309e067b7d6c79e0698630561e65f889913094ed
Mike <mchavez3@uw.edu>
2019-05-06 17:07:10 -0700

    Add code to drop rows w/ missing data

--- HW3/eval.py
+++ HW3/eval.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn import metrics
 
 imp = pd.read_csv('training_with_imputed.csv')
@@ -6,6 +7,29 @@ grnd = pd.read_csv('training_groundtruth.csv')
 
 print(imp.info())
 print(grnd.info())
+
+print(imp.index)
+
+
+
+# Get indices with missing data in ground truth
+indices_missing = pd.isnull(grnd).any(1).nonzero()[0]
+
+# Check shape of np array
+print(np.shape(indices_missing))
+
+# Convert NP array to PD DataFrame
+indices_missing = pd.DataFrame(data=indices_missing.flatten(),columns=['missing_rows'])
+print(indices_missing)
+
+# Delete rows with missing data
+imp = imp.drop(indices_missing['missing_rows'])
+grnd = grnd.drop(indices_missing['missing_rows'])
+
+# Validate Delete was successful
+print(imp.info())
+print(grnd.info())
+
 
 # not yet working
 for column in imp:
